d61f293b3459aca135242c7a1ce975ebfd966dd3
José R. Gutierrez <josefo727@gmail.com>
2024-12-05 09:10:53 -0500

    Add auto-open on new notifications
    Implemented a watch on the unread notifications count to automatically open the dropdown menu when a new unread notification is added. This enhances user experience by ensuring immediate visibility of updates. Also added an ID to the dropdown button for querying it in the new watch feature.

--- resources/js/Components/Notifications.vue
+++ resources/js/Components/Notifications.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import {ref, onMounted, watch} from 'vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import { Inertia } from "@inertiajs/inertia";
 
@@ -35,12 +35,20 @@ const navigateTo = (notificationId) => {
 const updateUnreadNotificationsCount = () => {
     unreadNotificationsCount.value = notifications.value.filter(n => !n.read).length;
 }
+
+watch(unreadNotificationsCount, (newVal, oldVal) => {
+    if (newVal > oldVal) {
+        const dropdownButton = document.querySelector('#dropdownButton');
+        if (dropdownButton)
+            dropdownButton.click();
+    }
+});
 </script>
 
 <template>
     <Dropdown align="right" width="96" :key="'notifications-menu'">
         <template #trigger>
-            <button class="relative inline-flex items-center">
+            <button id="dropdownButton" class="relative inline-flex items-center">
                 <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118.6 14.6L15 11V7h1a2 2 0 100-4h-4a2 2 0 000 4h1v4.345M5 18.5a1.5 1.5 0 11-3 0H5z" />
                 </svg>
